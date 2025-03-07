f76add0109f5405878ebe3591142709f5f0e4679
GaloMartinez <114940307+GaloMartinez@users.noreply.github.com>
2025-03-07 14:18:58 -0300

    commit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -22,7 +22,7 @@ const Navbar: React.FC = () => {
       }`}
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8 }}
+      transition={{ duration: 0.6 }}
     >
      
       <a href="#inicio" className="text-3xl font-bold text-gray-900">
